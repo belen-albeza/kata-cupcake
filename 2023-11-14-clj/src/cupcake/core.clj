05ea76c3c19e5039f49e72baad4c73fb342e9ff7
Belén Albeza <belen@hey.com>
2023-11-14 13:16:15 +0100

    implement description for one topping

--- 2023-11-14-clj/src/cupcake/core.clj
+++ 2023-11-14-clj/src/cupcake/core.clj
@@ -2,13 +2,36 @@
   (:gen-class))
 
 (defn cupcake []
-  {:desc "🧁"})
+  {:desc "🧁" :toppings []})
 
 (defn cookie []
-  {:desc "🍪"})
+  {:desc "🍪" :toppings []})
+
+(defn- toppings-desc [toppings]
+  {:pre [(vector? toppings) (every? #(some? (:desc %)) toppings)]}
+  (:desc (first toppings)))
 
 (defn desc [cake]
-  (:desc cake))
+  (if (empty? (:toppings cake))
+    (:desc cake)
+    (let [toppings (toppings-desc (:toppings cake))]
+      (str (:desc cake) " with " toppings))))
+
+(defn- add-topping [cake topping]
+  {:pre (vector? (:toppings cake))}
+  (let [
+    toppings (:toppings cake)
+    next-toppings (conj toppings {:desc topping})]
+    (assoc cake :toppings next-toppings)))
+
+(defn chocolate [cake]
+  (add-topping cake "🍫"))
+
+(defn nuts [cake]
+  (add-topping cake "🌰"))
+
+(defn sugar [cake]
+  (add-topping cake "🍬"))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- 2023-11-14-clj/test/cupcake/core_test.clj
+++ 2023-11-14-clj/test/cupcake/core_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [cupcake.core :refer :all]))
 
-(deftest base-cakes
+(deftest base-cakes-test
   (testing "Returns the description for a bare cupcake"
     (let [c (cupcake)]
       (is (= (desc c) "🧁"))))
@@ -10,3 +10,16 @@
   (testing "Returns the description for a bare cookie"
     (let [c (cookie)]
       (is (= (desc c) "🍪")))))
+
+(deftest single-topping-test
+  (testing "Returns the description for a cake with chocolate"
+    (let [c (chocolate(cookie))]
+      (is (= (desc c) "🍪 with 🍫"))))
+
+  (testing "Returns the description for a cake with nuts"
+    (let [c (nuts(cupcake))]
+      (is (= (desc c) "🧁 with 🌰"))))
+
+  (testing "Returns the description for a cake with sugar"
+    (let [c (sugar(cupcake))]
+      (is (= (desc c) "🧁 with 🍬")))))
